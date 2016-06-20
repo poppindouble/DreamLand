2eafca5822cae108b0728194ac844c995a2046e1
Shuangshuang Zhao <poppindouble@gmail.com>
2016-06-19 23:19:46 -0400

    add font-awesome

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -8,47 +8,52 @@ export default class Projects extends Component {
 				<h1>{this.props.backgrounColour}</h1>
 
 				<div className="project-item">
-					<img src='../img/off-the-grid.jpg' />
-					<div className='prject-item-overlay'>
 
+					<div className='prject-item-overlay'>
+						<h3>Project Name</h3>
+						<i className="fa fa-github-alt" aria-hidden="true"></i>
 					</div>
 				</div>
 				<div className="project-item">
 					<img src='../img/off-the-grid.jpg' />
 					<div className='prject-item-overlay'>
-
-					</div>
-
-				</div>
-				<div className="project-item">
-					<img src='../img/off-the-grid.jpg' />
-					<div className='prject-item-overlay'>
-
-					</div>
-
-				</div>
-				<div className="project-item">
-					<img src='../img/off-the-grid.jpg' />
-					<div className='prject-item-overlay'>
-
+						<h3>Project Name</h3>
+						<i className="fa fa-github-alt" aria-hidden="true"></i>
 					</div>
 				</div>
 				<div className="project-item">
 					<img src='../img/off-the-grid.jpg' />
 					<div className='prject-item-overlay'>
-
+						<h3>Project Name</h3>
+						<i className="fa fa-github-alt" aria-hidden="true"></i>
 					</div>
 				</div>
 				<div className="project-item">
 					<img src='../img/off-the-grid.jpg' />
 					<div className='prject-item-overlay'>
-
+						<h3>Project Name</h3>
+						<i className="fa fa-github-alt" aria-hidden="true"></i>
 					</div>
 				</div>
 				<div className="project-item">
 					<img src='../img/off-the-grid.jpg' />
 					<div className='prject-item-overlay'>
-
+						<h3>Project Name</h3>
+						<i className="fa fa-github-alt" aria-hidden="true"></i>
+					</div>
+				</div>
+				<div className="project-item">
+					<img src='../img/off-the-grid.jpg' />
+					<div className='prject-item-overlay'>
+						<h3>Project Name</h3>
+						<i className="fa fa-github-alt" aria-hidden="true"></i>
+					</div>
+				</div>
+				<div className="project-item">
+					<img src='../img/off-the-grid.jpg' />
+					<div className='prject-item-overlay'>
+						<h3>Project Name</h3>
+						<i className="fa fa-github-alt" aria-hidden="true"></i>
 					</div>
 				</div>
 			</div>
