b0c3e66d251398190e3b4b8ee9688ace102b7d94
Shuangshuang Zhao <poppindouble@gmail.com>
2016-06-20 00:23:08 -0400

    modified css

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -8,51 +8,111 @@ export default class Projects extends Component {
 				<h1>{this.props.backgrounColour}</h1>
 
 				<div className="project-item">
+					<img src='../img/off-the-grid.jpg' />
+					<div className='prject-item-overlay'>
+						<h3>Project Name</h3>
+						<p>Description</p>
+						<a href="https://github.com/davidozhang/off-the-grid">
+							<i className="fa fa-github-alt" aria-hidden="true"></i>
+						</a>
+					</div>
+				</div>
 
-					<div className='prject-item-overlay'>
-						<h3>Project Name</h3>
-						<i className="fa fa-github-alt" aria-hidden="true"></i>
-					</div>
-				</div>
 				<div className="project-item">
 					<img src='../img/off-the-grid.jpg' />
 					<div className='prject-item-overlay'>
 						<h3>Project Name</h3>
+						<p>Description</p>
 						<i className="fa fa-github-alt" aria-hidden="true"></i>
 					</div>
 				</div>
+
 				<div className="project-item">
 					<img src='../img/off-the-grid.jpg' />
 					<div className='prject-item-overlay'>
 						<h3>Project Name</h3>
+						<p>Description</p>
 						<i className="fa fa-github-alt" aria-hidden="true"></i>
 					</div>
 				</div>
+
 				<div className="project-item">
 					<img src='../img/off-the-grid.jpg' />
 					<div className='prject-item-overlay'>
 						<h3>Project Name</h3>
+						<p>Description</p>
 						<i className="fa fa-github-alt" aria-hidden="true"></i>
 					</div>
 				</div>
+
 				<div className="project-item">
 					<img src='../img/off-the-grid.jpg' />
 					<div className='prject-item-overlay'>
 						<h3>Project Name</h3>
+						<p>Description</p>
 						<i className="fa fa-github-alt" aria-hidden="true"></i>
 					</div>
 				</div>
+
 				<div className="project-item">
 					<img src='../img/off-the-grid.jpg' />
 					<div className='prject-item-overlay'>
 						<h3>Project Name</h3>
+						<p>Description</p>
 						<i className="fa fa-github-alt" aria-hidden="true"></i>
 					</div>
 				</div>
+
 				<div className="project-item">
 					<img src='../img/off-the-grid.jpg' />
 					<div className='prject-item-overlay'>
 						<h3>Project Name</h3>
+						<p>Description</p>
+						<i className="fa fa-github-alt" aria-hidden="true"></i>
+					</div>
+				</div>
+
+				<div className="project-item">
+					<img src='../img/off-the-grid.jpg' />
+					<div className='prject-item-overlay'>
+						<h3>Project Name</h3>
+						<p>Description</p>
+						<i className="fa fa-github-alt" aria-hidden="true"></i>
+					</div>
+				</div>
+
+				<div className="project-item">
+					<img src='../img/off-the-grid.jpg' />
+					<div className='prject-item-overlay'>
+						<h3>Project Name</h3>
+						<p>Description</p>
+						<i className="fa fa-github-alt" aria-hidden="true"></i>
+					</div>
+				</div>
+
+				<div className="project-item">
+					<img src='../img/off-the-grid.jpg' />
+					<div className='prject-item-overlay'>
+						<h3>Project Name</h3>
+						<p>Description</p>
+						<i className="fa fa-github-alt" aria-hidden="true"></i>
+					</div>
+				</div>
+
+				<div className="project-item">
+					<img src='../img/off-the-grid.jpg' />
+					<div className='prject-item-overlay'>
+						<h3>Project Name</h3>
+						<p>Description</p>
+						<i className="fa fa-github-alt" aria-hidden="true"></i>
+					</div>
+				</div>
+
+				<div className="project-item">
+					<img src='../img/off-the-grid.jpg' />
+					<div className='prject-item-overlay'>
+						<h3>Project Name</h3>
+						<p>Description</p>
 						<i className="fa fa-github-alt" aria-hidden="true"></i>
 					</div>
 				</div>
