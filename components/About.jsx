292a6e8e541519c0862add6ecd669b8aa6b6695a
Shuangshuang Zhao <poppindouble@gmail.com>
2016-06-26 00:31:51 -0400

    backup before I get high

--- components/About.jsx
+++ components/About.jsx
@@ -4,9 +4,9 @@ export default class About extends Component {
 	render() {
 		return (
 			<div className='about-header'>
-				<h1>Shuangshuang Zhao</h1>
-				<h2>iOS, Front-End Developer</h2>
-				<img src="../img/profile.jpg" />
+				<h1 className='about-name'>Shuangshuang Zhao</h1>
+				<h2 className='about-title'>iOS, Front-End Developer</h2>
+				<img className='about-img' src="../img/profile.jpg" />
 			</div>
 		);
 	}
