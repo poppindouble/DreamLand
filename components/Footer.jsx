80a05195864b21eb7f00abd5c78b5be792aeef77
Shuangshuang Zhao <poppindouble@gmail.com>
2016-06-25 22:57:44 -0400

    add facebook button at footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 
 export default class Footer extends Component {
+
+	onLinkedInClicked() {
+		console.log("Linked in got clicked")
+	}
+
+	onFacebookClicked() {
+		console.log("facebook got clicked")
+	}
+
 	render() {
 		return (
 			<footer className="footer">
-				<button onClick={this.onProjectClicked}>
+				<button onClick={this.onLinkedInClicked}>
 					<i className="fa fa-linkedin-square" aria-hidden="true"></i>
+				</button>
+				<button onClick={this.onFacebookClicked}>
+					<i className="fa fa-facebook-official" aria-hidden="true"></i>
 				</button>
 			</footer>
 		);
