b976572af2ee40caaa3937e487dae3e1989dc8e4
Shuangshuang Zhao <poppindouble@gmail.com>
2016-07-07 23:42:51 -0400

    set inital state of canvas

--- sketch.js
+++ sketch.js
@@ -2,8 +2,16 @@ function setup() {
 	var myCanvas = createCanvas(windowWidth, windowHeight);
  	myCanvas.parent('myCanvas');
  	background(255);
+ 	for(var x = 0; x <= myCanvas.width; x = x + 20) {
+ 		for(var y = 0; y <= myCanvas.height; y = y + 20) {
+ 			strokeWeight(0);
+ 			fill(128, 128, 128);
+ 			ellipse(x, y, 3);
+ 		}
+ 	}
+
+ 	console.log("wtf")
 }
 
 function draw() {
-  console.log("wtf")
 }
